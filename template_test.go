86734bc0ade9326da09a0eb8b22e1688c1847a54
arturbartolelli <arturbartonelli@gmail.com>
2025-12-01 10:45:03 -0300

    added filters to ListTemplate

--- template_test.go
+++ template_test.go
@@ -4,12 +4,40 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	whatsapp_business "github.com/verbeux-ai/whatsapp-business"
 )
 
 func TestClient_ListTemplates(t *testing.T) {
-	result, err := client.ListTemplates(t.Context())
-	require.NoError(t, err)
-	require.NotEmpty(t, result)
+	t.Run("Without filter", func(t *testing.T) {
+		result, err := client.ListTemplates(t.Context(), whatsapp_business.ListTemplateFilter{})
+		require.NoError(t, err)
+		require.NotEmpty(t, result.Data)
+	})
+
+	t.Run("With limit", func(t *testing.T) {
+		result, err := client.ListTemplates(t.Context(), whatsapp_business.ListTemplateFilter{
+			Limit: 1,
+		})
+		require.NoError(t, err)
+		require.Len(t, result.Data, 1)
+	})
+
+	t.Run("With after cursor", func(t *testing.T) {
+		result, err := client.ListTemplates(t.Context(), whatsapp_business.ListTemplateFilter{
+			Limit: 1,
+		})
+		require.NoError(t, err)
+		require.Len(t, result.Data, 1)
+		require.NotEmpty(t, result.Paging.Cursors.After)
+
+		result, err = client.ListTemplates(t.Context(), whatsapp_business.ListTemplateFilter{
+			Limit: 1,
+			After: result.Paging.Cursors.After,
+		})
+
+		require.NoError(t, err)
+		require.Len(t, result.Data, 1)
+	})
 }
 
 func TestClient_GetTemplate(t *testing.T) {
